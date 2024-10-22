c45dd1f683d8609c859af50a59cef8cf15c7ed4b
ЕвгенийК <widgetev@gmail.com>
2024-10-22 14:31:06 +0300

    Задача 6. запрос продуктов по пользователю

--- payment-service/src/main/java/org/example/controller/PaymentController.java
+++ payment-service/src/main/java/org/example/controller/PaymentController.java
@@ -1,10 +1,10 @@
 package org.example.controller;
 
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.example.service.PaymentService;
@@ -16,17 +16,12 @@ public class PaymentController {
     private static final Logger log = LoggerFactory.getLogger(PaymentController.class.getName());
 
     private final PaymentService paymentService;
-    private final RestTemplate restTemplate;
-
-    public PaymentController(PaymentService paymentService
-                            , RestTemplate restTemplate) {
+    public PaymentController(PaymentService paymentService) {
         this.paymentService = paymentService;
-        this.restTemplate = restTemplate;
     }
 
-    @GetMapping("/products")
-    public String getProducts(@Value("${service.products.url}") String paymentsUrl){
-        log.info("Go for products {}",paymentsUrl);
-        return restTemplate.postForObject(paymentsUrl,null, String.class);
+    @GetMapping("/user/{userId}/products")
+    public String getUserProducts(@Value("${service.products.byuser.url}") String paymentsUrl, @PathVariable Long userId){
+        return paymentService.getProductByUser(paymentsUrl,userId);
     }
 }
--- payment-service/src/main/java/org/example/service/PaymentService.java
+++ payment-service/src/main/java/org/example/service/PaymentService.java
@@ -1,8 +1,37 @@
 package org.example.service;
 
+import org.example.controller.PaymentController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.client.RestOperations;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
+    private static final Logger log = LoggerFactory.getLogger(PaymentController.class.getName());
+    private final RestTemplate restTemplate;
+    public PaymentService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
+    public String getProductByUser(String paymentsUrl, @PathVariable Long userId) {
+
+        //подготовить параметр
+        Map<String, String> urlParams = new HashMap<>();
+        urlParams.put("id", userId.toString());
+
+        //Сформировать uri
+        URI uri = UriComponentsBuilder.fromUriString(paymentsUrl)
+                .buildAndExpand(urlParams).toUri();
+        log.info("request for {}", uri);
+
+        return restTemplate.postForObject(uri, null, String.class);
+    }
 }
